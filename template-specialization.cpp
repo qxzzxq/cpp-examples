51297742f099a001b053c2ee62cdd69dd21b24c5
Xuzhou Qin <17144939+qxzzxq@users.noreply.github.com>
2020-11-06 11:48:02 +0100

    Update template-specialization.cpp

--- template-specialization.cpp
+++ template-specialization.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 
-template <class T, int size>
+class Object {
+private:
+    char m_data;
+public:
+    Object() : m_data{} {
+    }
+
+    explicit Object(char data) : m_data{data} {
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Object &obj) {
+        out << obj.m_data;
+        return out;
+    }
+};
+
+template<class T, int size>
 class BaseStorage {
 protected:
     T m_data[size];
     int m_length{size};
 
-    BaseStorage(): m_data{} {
-        std::cout << "Initialise BaseStorage of type \""<< typeid(T).name() <<"\" with length " << m_length << '\n';
+    BaseStorage() : m_data{} {
+        std::cout << "Initialise BaseStorage of type \"" << typeid(T).name() << "\" with length " << m_length << '\n';
     }
+
+    void checkIndex(int index) {
+        if (index >= m_length || index < 0) {
+            std::cout << "Invalid index " << index << '\n';
+            exit(1);
+        }
+    }
+
 public:
 
-    virtual BaseStorage& set(T value, int index) {
+    virtual BaseStorage &set(T value, int index) {
+        checkIndex(index);
         m_data[index] = value;
         return *this;
     }
@@ -24,7 +49,7 @@ public:
         return m_data[index];
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const BaseStorage& str) {
+    friend std::ostream &operator<<(std::ostream &out, const BaseStorage &str) {
         out << "BaseStorage data: [";
         for (int i{0}; i < str.m_length; ++i) {
             T data{str.m_data[i]};
@@ -39,11 +64,10 @@ public:
     }
 };
 
-template <class T, int size>
+template<class T, int size>
 class Storage : public BaseStorage<T, size> {
-
 public:
-    Storage(): BaseStorage<T, size>() {
+    Storage() : BaseStorage<T, size>() {
         std::cout << "Initialise Storage\n";
     }
 
@@ -51,7 +75,7 @@ public:
         std::cout << "Destruct Storage\n";
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const Storage& str) {
+    friend std::ostream &operator<<(std::ostream &out, const Storage &str) {
         out << "Storage data: [";
         for (int i{0}; i < str.m_length; ++i) {
             T data{str.m_data[i]};
@@ -67,20 +91,17 @@ public:
 
 };
 
-template <class T, int size>
-class Storage<T*, size> : public BaseStorage<T*, size>  {
-
+template<class T, int size>
+class Storage<T *, size> : public BaseStorage<T *, size> {
 public:
-
-    Storage(): BaseStorage<T*, size>{} {
+    Storage() : BaseStorage<T *, size>{} {
         std::cout << "Initialise Storage (pointer specialized)\n";
-
     }
 
-    friend std::ostream& operator<<(std::ostream& out, const Storage& str) {
+    friend std::ostream &operator<<(std::ostream &out, const Storage &str) {
         out << "Storage data: [";
         for (int i{0}; i < str.m_length; ++i) {
-            T* data{str.m_data[i]};
+            T *data{str.m_data[i]};
             if (i == str.m_length - 1) {
                 out << *data;
             } else {
@@ -92,20 +113,32 @@ public:
     }
 };
 
-class Object {
-private:
-    char m_data;
+// Object class specialized
+template<int size>
+class Storage<Object, size> : public BaseStorage<Object, size> {
 public:
-    Object(): m_data{} {
-
+    friend std::ostream &operator<<(std::ostream &out, const Storage &str) {
+        out << "Storage data: ";
+        for (int i{0}; i < str.m_length; ++i) {
+            Object data{str.m_data[i]};
+            out << data;
+        }
+        out << '\n';
+        return out;
     }
+};
 
-    explicit Object(char data): m_data{data} {
-
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const Object& obj) {
-        out << obj.m_data;
+// Object pointer specialized
+template<int size>
+class Storage<Object *, size> : public BaseStorage<Object *, size> {
+public:
+    friend std::ostream &operator<<(std::ostream &out, const Storage &str) {
+        out << "Storage data: ";
+        for (int i{0}; i < str.m_length; ++i) {
+            Object *data{str.m_data[i]};
+            out << *data;
+        }
+        out << '\n';
         return out;
     }
 };
@@ -116,19 +149,27 @@ int main() {
     intStorage.set(1, 0);
     intStorage.set(2, 1);
     intStorage.set(3, 2);
-
     std::cout << intStorage << "==================\n";
 
+    Storage<bool, 3> boolStorage{};
+    boolStorage.set(true, 0);
+    boolStorage.set(true, 1);
+    boolStorage.set(true, 2);
+    std::cout << boolStorage << "==================\n";
 
-    Storage<Object, 2> objStorage{};
+    Storage<Object, 5> objStorage{};
     objStorage.set(Object{'h'}, 0);
-    objStorage.set(Object{'a'}, 1);
-
+    objStorage.set(Object{'e'}, 1);
+    objStorage.set(Object{'l'}, 2);
+    objStorage.set(Object{'l'}, 3);
+    objStorage.set(Object{'o'}, 4);
     std::cout << objStorage << "==================\n";
 
-    Storage<Object*, 2> objStorage2{};
+    Storage<Object *, 2> objStorage2{};
     objStorage2.set(new Object{'w'}, 0);
     objStorage2.set(new Object{'a'}, 1);
-
     std::cout << objStorage2 << "\n==================\n";
+
+    // invalid index
+    objStorage.set(Object{'o'}, 50);
 }
